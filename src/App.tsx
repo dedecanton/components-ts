749b60285e38b45c25ee308d757f3656e58f5d34
dedecanton <andrecanton18@gmail.com>
2022-05-10 18:30:14 -0300

    feat: footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Footer from "./components/Footer/";
 import Header from "./components/Header";
 import About from "./sections/About";
 import Home from "./sections/Home";
@@ -16,7 +17,7 @@ function App() {
       <Testimonial/>      
       {/* Galeria */}
       <Location/>
-      {/* Footer */}
+      <Footer/>
     </div>
   );
 }
